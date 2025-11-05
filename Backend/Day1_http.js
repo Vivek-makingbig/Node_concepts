0d992ef0d046bb81f46441a072c8c0eeb8680b06
Vivek Kumar shaw <vivekshaw8420@gmail.com>
2025-11-05 16:12:18 +0530

    Uncomment server code for HTTP routing

--- Backend/Day1_http.js
+++ Backend/Day1_http.js
@@ -1,24 +1,24 @@
 // Method 1 of creating a server
 // Creating a basic http server and perform routing
-// const http = require('http');
-// const server = http.createServer((req,res)=>
-// {
-//     if(req.url=='/'){
-//     res.writeHead(200,{'Content-Type':'text/plain'});
-//     res.end('Welcome to home page');
-//     }
-//     else if(req.url=='/about'){
-//     res.writeHead(200,{'Content-Type':'text/plain'});
-//     res.end('Welcome to About page');
-//     }
-//     else
-//     {
+const http = require('http');
+const server = http.createServer((req,res)=>
+{
+    if(req.url=='/'){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Welcome to home page');
+    }
+    else if(req.url=='/about'){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Welcome to About page');
+    }
+    else
+    {
        
-//     res.writeHead(404,{'Content-Type':'text/plain'});
-//     res.end('Page not found');
+    res.writeHead(404,{'Content-Type':'text/plain'});
+    res.end('Page not found');
     
-//     }
-// });
+    }
+});
 // const PORT = 3000;
 // server.listen(PORT,()=>{
 //     console.log('Server running on port : 3000');
