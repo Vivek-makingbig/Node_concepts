8437849f8f4b15ba15ff3b5a1ba4ba4c5fc84b3d
Vivek kumar shaw <vivekshaw8420@gmail.com>
2025-11-07 18:45:40 +0530

    first commit

---
+++ Backend/Day2_File_reading.js
@@ -0,0 +1,15 @@
+//Synchronous way of reading the content of a file
+// const fs = require('fs');
+// const data = fs.readFileSync('Mydetails.txt','utf-8');
+// console.log('The content of the file is :\n');
+// console.log(data);
+
+
+//Asynchronous way of reading the content of a file
+const fs = require('fs');
+ fs.readFile('Mydetails.txt','utf-8',(err,data)=>{
+    if (err)  throw err
+    console.log(data)
+})
+
+
